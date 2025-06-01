7f23b16f800c6e11128383799c2ab7486f37ba4d
João Vitor Silva <joaovitorssilva007@gmail.com>
2025-06-01 20:31:17 -0300

    main dinamico

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,22 @@
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
         tabelaHash hash = new tabelaHash(26);
 
-        String caminhoTexto = "C:\\Users\\joaov\\OneDrive\\Documentos\\Faculdade\\IndiceRemissivo\\teste\\texto.txt";
+        String caminhoTexto = "C:\\Users\\joaov\\OneDrive\\Documentos\\Faculdade\\Estrutura de Dados\\IndiceRemissivo\\teste\\texto.txt";
         leitorArquivo.processarTexto(caminhoTexto, hash);
 
         System.out.println("indices remissivos:");
         hash.imprimirIndiceRemissivo();
 
-        String caminhoSainda = "C:\\Users\\joaov\\OneDrive\\Documentos\\Faculdade\\IndiceRemissivo\\teste\\indice.txt";
-        hash.exportarIndiceRemissivo(caminhoSainda);
+        String pastaDestino = "C:\\Users\\joaov\\OneDrive\\Documentos\\Faculdade\\Estrutura de Dados\\IndiceRemissivo\\resultado";
+
+        String nomeArquivo = "resultado.txt";
+        File arquivoSaida = new File(pastaDestino, nomeArquivo);
+
+        hash.exportarIndiceRemissivo(arquivoSaida.getAbsolutePath());
+
+        System.out.println("indice salvo em: " + arquivoSaida.getAbsolutePath());
     }
 }
